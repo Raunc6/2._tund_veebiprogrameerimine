f8ca9c92dd39632fa838e853512c596d327a8606
Rauno taaksalu <rauntaa@greeny.cs.tlu.ee>
2015-09-11 11:36:29 +0300

    logic added

--- hello_world.php
+++ hello_world.php
@@ -5,3 +5,21 @@
 	echo $first_name." ".$last_name;
 	
 ?>	
+
+<br>
+
+<?php
+
+	$age = 20;
+	//kui on väiksem kui 18, siis alaealine
+	//Kui on suurem või võrdne 18-ega, siis täisealine.
+	if($age < 18) {
+		//tõene
+		echo "alaealine";
+		
+	} else {
+		//väär
+		echo "täisealine";
+	}
+
+?>
